bc2f6de47b901911d192cbc36143acaef730a6dd
AMohammedShafeek <amdshafeek7@gmail.com>
2026-06-14 16:54:16 +0530

    feat: connect the DB andd get the DB health by /health endpoint

--- 01-password-hashing/app/main.py
+++ 01-password-hashing/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.db.database import engine
 
 app = FastAPI()
 
@@ -6,3 +8,18 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "01-password-hashing"}
+
+@app.get("/health")
+def health():
+    with engine.connect() as connection:
+        result = connection.execute(
+            text("SELECT current_database();")
+        )
+
+        db_name = result.scalar()
+    return {
+        "database": db_name,
+        "status": "true",
+        "mesage": "DB Connected"
+    }
+    
